08b295bf9b68337228363e216352df90c115f8b4
singh0471 <142919646+singh0471@users.noreply.github.com>
2024-09-30 13:40:20 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Game = require("./game/game.js");
+
+const game1 = Game.newGame("abcd","xyz");
+game1.play(0);
+game1.play(8);
+game1.play(4);
+game1.play(2);
+game1.play(6);
+game1.play(2);
+game1.play(1);
+game1.play(5);
+game1.play(7);
+game1.play(3);
